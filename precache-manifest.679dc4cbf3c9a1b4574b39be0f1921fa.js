dd4f5f183b582e5acb694240d2a5ffdb103ec1b7
jaganswornkar <jaganswornkar1998@gmail.com>
2020-02-20 04:40:25 +0530

    Updates

---
+++ precache-manifest.679dc4cbf3c9a1b4574b39be0f1921fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cfcd681be9b3eb303a90c3ac43b468",
+    "url": "/youtube-playlist/index.html"
+  },
+  {
+    "revision": "c7947381fa199195530e",
+    "url": "/youtube-playlist/static/css/main.da326a8e.chunk.css"
+  },
+  {
+    "revision": "d498af955eb45e57c81d",
+    "url": "/youtube-playlist/static/js/2.43cfafa2.chunk.js"
+  },
+  {
+    "revision": "256b892e56b7417b1bb9cd4d8ba69983",
+    "url": "/youtube-playlist/static/js/2.43cfafa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7947381fa199195530e",
+    "url": "/youtube-playlist/static/js/main.14e67a18.chunk.js"
+  },
+  {
+    "revision": "69a1a2f63d0d4c84401e",
+    "url": "/youtube-playlist/static/js/runtime-main.0faee72b.js"
+  }
+]);
